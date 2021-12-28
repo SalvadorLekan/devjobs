3f05065c2ca132b5dcc6db0a64b71ce1c2ac70e8
Toriola Faidat <faaedah@gmail.com>
2021-12-28 14:25:19 +0100

    added inner page

--- js/main.js
+++ js/main.js
@@ -6,8 +6,10 @@ const filter = document.querySelector('.filter')
 const searchTitle = document.querySelector('.search-input')
 const inputLocation = document.querySelector('.input-location')
 const checkbox = document.querySelector('.checkbox');
+const innerPage = document.querySelector('.inner-page')
+//const mainPage = document.querySelector('.main-page')
 
-
+const inputWrap = document.querySelector('.input-wrap')
 const detailContainer = document.querySelector('.detail-container')
 
 //let title = searchTitle.value;
@@ -30,7 +32,9 @@ function renderJobs (data) {
             const allJobs = [...mainPage.children]  
             allJobs.forEach( (jobs) => {
                 jobs.addEventListener('click', () => {
-                    detailContainer.style.display = 'block'
+                    innerPage.style.display = 'block';
+                    mainPage.style.display = 'none'
+                    inputWrap.style.display = 'none'
                     getDetails(job);
                 });
             })
@@ -39,10 +43,74 @@ function renderJobs (data) {
 
 function getDetails (job) {
     detailContainer.innerHTML += `
+    <div>
     <div class="detail">
-        <p class="blue">${job.description}</p>
-        <p class="blue">${job.apply}</p>
-    </div> 
+        <div class="details-box">
+            <div>
+                <div class="company-wrap">
+                    <div class="company-logo-inner flex-center" style="style="background-color: ${job.logoBackground};">
+                        <img src="${job.logo}" alt="company-logo" class="inner-logo"/>
+                    </div>
+                    <div>
+                        <p class="company-name">${job.company}</p>
+                        <p>
+                            <a href="${job.website}">${job.company}.com</a>
+                        </p>
+                    </div>
+                </div>
+                <button class="company-site">
+                    <a href="${job.website}">Company Site</a>
+                </button>
+            </div>
+        </div>
+
+            <div class="description-container">
+                <div class="description">
+                    <div>
+                        <p class="notes">${job.postedAt}
+                            <span class="dot">.</span>
+                            <span>${job.contract}</span>
+                        </p>
+                        <h1>${job.position}</h1>
+                        <h2 class="job-location">${job.location}</h2>
+                    </div>
+                    
+                    <button class="apply-now">
+                        <a href="${job.apply}">Apply Now</a>
+                    </button>
+                </div>
+           
+            <p class="notes">${job.description}</p>
+
+            <h2 class="black-h2">Requirements</h2>
+            <p class="notes">${job.requirements.content}</p>
+            
+            <ul>
+                <li>Morbi interdum mollis sapien. Sed</li>
+                <li>Morbi interdum mollis sapien. Sed</li>
+                <li>Morbi interdum mollis sapien. Sed</li>
+            </ul>
+
+            <h2 class="black-h2">What You Will Do</h2>
+            <p class="notes">${job.role.content}</p>
+
+            <ol>
+                <li>Morbi interdum mollis sapien. Sed</li>
+                <li>Morbi interdum mollis sapien. Sed</li>
+                <li>Morbi interdum mollis sapien. Sed</li>
+            </ol>
+        </div>
+        <div class="footer footer-tab">
+            <div class="hide-mobile">
+                <h2 class="black-h2">${job.position}</h2>
+                <p>${job.company}</p>
+            </div>
+            <button>
+                <a href>Apply Now</a>
+            </button>
+        </div>
+    </div>
+    </div>
     `
 }
 
@@ -120,7 +188,5 @@ function filterJobContract () {
                 }
             })
         }
-    })
-
-   
+    })  
 }
